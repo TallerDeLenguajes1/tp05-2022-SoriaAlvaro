b81f08572a6ce3ef2be2535faf8bb22e70d8c02d
Soria Alvaro <alvarosoria.universidad@gmail.com>
2022-05-23 22:51:47 -0300

    Calculadora V2, terminada

--- Program.cs
+++ Program.cs
@@ -11,9 +11,10 @@ namespace ejercicioUno
         {
             int op=0, numUno=0, numDos=0;
             Console.WriteLine("\tCalculadora\n1)Suma\n2)Resta\n3)Multiplicación\n4)División.\n");
+            Console.WriteLine("5)Valor Absoluto\n6)El Cuadrado de un num\n7)Raiz Cuadrada\n8)Sen\n9)Cos\n10)Parte entera de un decimal");
             do{
                 op= Convert.ToInt32(Console.ReadLine());
-                if((op <= 0 && op >= 5)){
+                if((op <= 0 && op >= 11)){
                     Console.WriteLine("Elija una opción correcta");
                 }
                 if(op == 1){
@@ -22,8 +23,7 @@ namespace ejercicioUno
                     numUno= Convert.ToInt32(Console.ReadLine());
                     Console.WriteLine("NUM DOS:");
                     numDos= Convert.ToInt32(Console.ReadLine());
-                    Console.WriteLine($"{numUno} + {numDos}");
-                    Console.WriteLine($"Resultado: {numUno + numDos}");
+                    Console.WriteLine($"{numUno} + {numDos} = {numUno + numDos}");
                 }
                 if(op == 2){
                     Console.WriteLine("\tResta");
@@ -31,8 +31,7 @@ namespace ejercicioUno
                     numUno= Convert.ToInt32(Console.ReadLine());
                     Console.WriteLine("NUM DOS:");
                     numDos= Convert.ToInt32(Console.ReadLine());
-                    Console.WriteLine($"{numUno} - {numDos}");
-                    Console.WriteLine($"Resultado: {numUno - numDos}");
+                    Console.WriteLine($"{numUno} - {numDos} = {numUno - numDos}");
                 }
                 if(op == 3){
                     Console.WriteLine("\tMultiplicación");
@@ -40,8 +39,7 @@ namespace ejercicioUno
                     numUno= Convert.ToInt32(Console.ReadLine());
                     Console.WriteLine("NUM DOS:");
                     numDos= Convert.ToInt32(Console.ReadLine());
-                    Console.WriteLine($"{numUno} * {numDos}");
-                    Console.WriteLine($"Resultado: {numUno * numDos}");
+                    Console.WriteLine($"{numUno} * {numDos} = {numUno * numDos}");
                 }
                 if(op == 4){
                     Console.WriteLine("\tDivisión");
@@ -49,31 +47,60 @@ namespace ejercicioUno
                     numUno= Convert.ToInt32(Console.ReadLine());
                     Console.WriteLine("NUM DOS:");
                     numDos= Convert.ToInt32(Console.ReadLine());
-                    Console.WriteLine($"{numUno} / {numDos}");
                     if(numDos != 0){
-                        Console.WriteLine($"Resultado: {numUno / numDos}");
+                        Console.WriteLine($"{numUno} / {numDos} = {Convert.ToDecimal(numUno / numDos)}");
                     }else{
-                        Console.WriteLine("Resultado: 0");
+                        Console.WriteLine($"{numUno} + {numDos} = 0");
                     }
                 }
+                if(op == 5){
+                    Console.WriteLine("\tValor Absoluto");
+                    Console.WriteLine("NUM:");
+                    numUno= Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine($"Valor Absoluto de {numUno} = {Convert.ToDecimal(Math.Abs(numUno))}");
+                }
+                if(op == 6){
+                    Console.WriteLine("\tEl Cuadrado de un num");
+                    Console.WriteLine("NUM:");
+                    numUno= Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine($"{numUno} Al Cuadrado = {Math.Pow(numUno, 2)}");
+                }
+                if(op == 7){
+                    double numR;
+                    Console.WriteLine("\tRaiz Cuadrada");
+                    Console.WriteLine("NUM:");
+                    numR= Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine($"Raiz Cuadrada de {numR} = {Math.Sqrt(numR)}");
+                }
+                if(op == 8){
+                    double numSin;
+                    Console.WriteLine("\tSen");
+                    Console.WriteLine("NUM:");
+                    numSin = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine($"Sen de {numSin} = {Math.Sin(numSin)}");
+                }
+                if(op == 9){
+                    double numCos;
+                    Console.WriteLine("\tCos");
+                    Console.WriteLine("NUM:");
+                    numCos = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine($"Cos de {numCos} = {Math.Cos(numCos)}");
+                }
+                if(op == 10){
+                    float numDec;
+                    Console.WriteLine("\tParte entera de un decimal");
+                    Console.WriteLine("NUM:");
+                    numDec = float.Parse(Console.ReadLine());
+                    Console.WriteLine($"Parte entera de {numDec} = {Math.Truncate(numDec)}");
+                }
+
+                Console.WriteLine("5)Valor Absoluto\n6)El Cuadrado de un num\n7)Raiz Cuadrada\n8)Sen\n9)Cos\n10)Parte entera de un decimal");
                 Console.WriteLine("Presiona ENTER para continuar");
                 Console.ReadKey();
                 Console.Clear();
                 Console.WriteLine("\tCalculadora\n1)Suma\n2)Resta\n3)Multiplicación\n4)División.\n");
-            }while(!(op <= 0 && op >= 5));
-        }
-        public static int reverso(int num){
-            int r=0, inv=0;
-            if(num > 10){
-               while(num != 0){
-                    r = num % 10;
-                    num = num / 10;
-                    inv = (inv * 10) + r;
-                }
-                num = inv;
-                return num;
-            }
-            return num;
+
+            }while(!(op <= 0 && op >= 11));
         }
     }
 }
